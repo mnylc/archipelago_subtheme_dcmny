e4ed4869689aa7155cdfa19bc4fb07e4abeb907b
Diego Pino Navarro <DiegoPino@users.noreply.github.com>
2025-02-20 12:45:54 -0500

    Try to capture clicks for Drupal's toolbar so we re-order masonry on that too

--- js/custom.js
+++ js/custom.js
@@ -57,7 +57,7 @@
 
         document.addEventListener('click', function (event) {
           // If the clicked element doesn't have the right selector, bail
-          if (!event.target.matches('#DcmnyLeftNavToggler')) return;
+          if (!event.target.matches('#DcmnyLeftNavToggler, #toolbar-bar .toolbar-item')) return;
           setTimeout(()=> {
               // Give the CSS transitions time to finish
               $grid.masonry('layout');
